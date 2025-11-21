324fedaaff846eabcc578f4a0fcbb0c92c0c5d93
JPLMB <145187137+JPLMB@users.noreply.github.com>
2025-11-21 08:37:38 -0300

    Script com lógica para mudar os sprites

--- mega-clone/scripts/player.gd
+++ mega-clone/scripts/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -17,6 +18,18 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite_2d.play("idle")
+		else: animated_sprite_2d.play("moving")
+	else: animated_sprite_2d.play("jump")
+	
+	if direction < 0:
+		animated_sprite_2d.flip_h = true
+	elif direction > 0:
+		animated_sprite_2d.flip_h = false
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
