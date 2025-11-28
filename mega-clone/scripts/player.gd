21ff1f10b341c477849d0a192293170db9e4d325
JPLMB <145187137+JPLMB@users.noreply.github.com>
2025-11-28 19:04:41 -0300

    Teste para corrigir os commits que deram errado

--- mega-clone/scripts/player.gd
+++ mega-clone/scripts/player.gd
@@ -5,8 +5,19 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var life_bar: AnimatedSprite2D = $"../HUD/Control/BoxContainer/LifeBar"
 
 func _physics_process(delta: float) -> void:
+	
+	# DEBUG -----------------------------------------
+	if Input.is_action_just_pressed("debug_damage"):
+		life_bar.take_damage_animated(1)
+
+	if Input.is_action_just_pressed("debug_heal"):
+		life_bar.heal(1)
+	# -----------------------------------------------
+
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
