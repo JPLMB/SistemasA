547b1fc1e245d9a7f8c8d1020a5910780559d2f8
JPLMB <145187137+JPLMB@users.noreply.github.com>
2025-12-12 08:40:20 -0300

    Boss
    Chefe básico adicionado, quando morre o orb aparece, orb por enquanto não faz nada

--- mega-clone/scripts/npc.gd
+++ mega-clone/scripts/npc.gd
@@ -1,15 +1,16 @@
 extends CharacterBody2D
 
-@export var speed: float = 0.0
-@export var health: int = 1
+@export var speed: float = 50.0
+@export var health: int = 3
+@export var direction_change_time := 2.0
 
-@export var sfx_hit: AudioStream
-@export var sfx_die: AudioStream
+var direction := -1
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var hitbox: Area2D = $Area2D
+@onready var direction_timer: Timer = null
 
-@export var orb_scene: PackedScene
+@export var sfx_hit: AudioStream
 
 func _spawn_sfx(stream: AudioStream):
 	if stream == null:
@@ -22,6 +23,7 @@ func _spawn_sfx(stream: AudioStream):
 	temp.top_level = true
 
 	get_tree().current_scene.add_child(temp)
+
 	temp.play()
 	temp.connect("finished", temp.queue_free)
 
@@ -29,14 +31,32 @@ func _ready():
 	hitbox.body_entered.connect(_on_body_entered)
 	hitbox.area_entered.connect(_on_area_entered)
 
+	# Garantir Timer
+	if has_node("DirectionTimer"):
+		direction_timer = $DirectionTimer
+	else:
+		direction_timer = Timer.new()
+		direction_timer.name = "DirectionTimer"
+		add_child(direction_timer)
+
+	direction_timer.wait_time = direction_change_time
+	direction_timer.timeout.connect(_on_direction_timer_timeout)
+	direction_timer.start()
+
 func _physics_process(_delta):
-	# Boss não se move, mas deixamos por consistência
-	velocity.x = 0
+	velocity.x = direction * speed
 	move_and_slide()
 
+	if is_on_wall():
+		_reverse_direction()
+
+func _on_direction_timer_timeout():
+	_reverse_direction()
+
 func _on_body_entered(body):
+	#print("Algo entrou na hitbox:", body)
 	if body.is_in_group("player"):
-		body.take_damage(4)
+		body.take_damage(3)
 
 func _on_area_entered(area):
 	if area.is_in_group("bullet"):
@@ -50,12 +70,8 @@ func take_damage():
 		die()
 
 func die():
-	_spawn_sfx(sfx_die)
+	queue_free()
 
-	# Spawn da ORB
-	if orb_scene:
-		var orb = orb_scene.instantiate()
-		orb.global_position = global_position
-		get_parent().call_deferred("add_child", orb)
-
-	call_deferred("queue_free")
+func _reverse_direction():
+	direction *= -1
+	sprite.flip_h = direction > 0
